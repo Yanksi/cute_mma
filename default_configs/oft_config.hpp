8928d5fed7883ba2e997fabaf9615edc25a5565f
Shuhao Li <shuhao.li@inf.ethz.ch>
2025-08-06 17:25:49 +0200

    new configuration file that is compatible with the latest version of code

--- default_configs/oft_config.hpp
+++ default_configs/oft_config.hpp
@@ -6,8 +6,10 @@ namespace cute {
         static const unsigned int bM = 128;
         static const unsigned int bN = 128;
         static const unsigned int bK = 32;
-        static const unsigned int bP1 = 2;
-        static const unsigned int bP2 = 2;
+        static const unsigned int c_width = 16;
+        static const unsigned int bP_a_r = 2;
+        static const unsigned int bP_ar = 2;
+        static const unsigned int bP_b = 2;
         using warp_layout1 = cute::Layout<cute::Shape<cute::Int<2>>>;
         using warp_layout2 = cute::Layout<cute::Shape<cute::Int<2>, cute::Int<2>>>;
     };
